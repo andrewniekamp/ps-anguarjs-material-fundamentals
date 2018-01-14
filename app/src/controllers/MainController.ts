5c76a2755d5f37cd413a05d33c30f4ae4cab7aa0
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-14 13:30:27 -0500

    Add service to controller and log demo data to js console

--- app/src/controllers/MainController.ts
+++ app/src/controllers/MainController.ts
@@ -2,8 +2,20 @@
 
 module ContactManagerApp {
     export class MainController {
-        static $inject = []; // Avoids issues with minification. In app, we will use a library for this
+        static $inject = ['userService']; // Avoids issues with minification. In app, we will use a library for this
         // Use controllerAs, avoid using $scope
+
+        constructor(private userService: IUserService) {
+            var vm = this;
+
+            vm.userService
+                .loadAllUsers()
+                .then( (users: User[]) => {
+                    vm.users = users;
+                    console.log(vm.users);
+                })
+        }
+
         message: string = 'Hello from our controller'; // Accessible from template with vm.message (using controllerAs vm)
     }
 }
