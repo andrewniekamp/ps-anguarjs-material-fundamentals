73dedb7bddc9034afa0423ff12f79bb932b3ff0f
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-14 13:14:08 -0500

    Add models from ajtowf template

--- app/src/controllers/MainController.ts
+++ app/src/controllers/MainController.ts
@@ -4,6 +4,6 @@ module ContactManagerApp {
     export class MainController {
         static $inject = []; // Avoids issues with minification. In app, we will use a library for this
         // Use controllerAs, avoid using $scope
-        // message: string = 'Hello from our controller'; // Accessible from template with vm.message (using controllerAs vm)
+        message: string = 'Hello from our controller'; // Accessible from template with vm.message (using controllerAs vm)
     }
 }
