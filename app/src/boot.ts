b20dd1c79be08ea6fe5bafc2c63518511f0753dd
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-14 14:38:03 -0500

    Add color themes with theme provider

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -4,8 +4,13 @@ module ContactManagerApp { // Not the app name, just for scoping purposes
     angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons'])
         .service('userService', UserService)
         .controller('mainController', MainController)
-        .config(($mdIconProvider: angular.material.IIconProvider) => {
+        .config((   $mdIconProvider: angular.material.IIconProvider,
+                    $mdThemingProvider: angular.material.IThemingProvider) => {
             $mdIconProvider
-                .icon('menu', './assets/svg/menu.svg', 24)
+                .icon('menu', './assets/svg/menu.svg', 24);
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue')
+                .accentPalette('red');
         })
 }
