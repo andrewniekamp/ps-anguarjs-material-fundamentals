9c61e1ab46a278315735a154abbc06b3f7907d61
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-14 14:33:15 -0500

    Replace reference to sidenavbar toggle icon with built in provider

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -3,5 +3,9 @@
 module ContactManagerApp { // Not the app name, just for scoping purposes
     angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons'])
         .service('userService', UserService)
-        .controller('mainController', MainController);
+        .controller('mainController', MainController)
+        .config(($mdIconProvider: angular.material.IIconProvider) => {
+            $mdIconProvider
+                .icon('menu', './assets/svg/menu.svg', 24)
+        })
 }
