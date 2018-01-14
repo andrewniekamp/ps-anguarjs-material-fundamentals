662aa14e74eb53bfb79add7ddea9886a0b83e4f0
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-14 14:26:06 -0500

    Add svg assets

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -1,7 +1,7 @@
 /// <reference path="_all.ts" />
 
 module ContactManagerApp { // Not the app name, just for scoping purposes
-    angular.module('contactManagerApp', ['ngMaterial'])
+    angular.module('contactManagerApp', ['ngMaterial', 'ngMdIcons'])
         .service('userService', UserService)
         .controller('mainController', MainController);
 }
